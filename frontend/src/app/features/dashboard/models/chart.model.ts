87a15712ce119a62b988837ae7142b3726863d1d
Lucas Almeida <lucas-lxx@alu.ufc.br>
2026-07-14 14:15:20 -0300

    delete: frotnend

--- frontend/src/app/features/dashboard/models/chart.model.ts
+++
@@ -1,1 +0,0 @@
-export type ChartType = 'by-category';
--- frontend/src/app/features/email-verified/email-verified.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { EmailVerifiedPageComponent } from './email-verified-page.component';
-
-export const EMAIL_VERIFIED_ROUTES: Routes = [
-  { path: '', component: EmailVerifiedPageComponent },
-];
